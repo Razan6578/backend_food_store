96e309402c52a2a1fdda1d5deda77919bc974b1e
Razan6578 <ngvozdik11@gmail.com>
2026-06-05 17:48:47 +0300

    added product list

--- food/serializers.py
+++ food/serializers.py
@@ -1,12 +1,10 @@
 from rest_framework import serializers
 from .models import Food, FoodImage
 
-
 class FoodImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = FoodImage
         fields = ['id', 'image']
-
 
 class FoodSerializer(serializers.ModelSerializer):
     images = FoodImageSerializer(many=True, read_only=True)
